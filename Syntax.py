b9c8f3f4eb8b5b16b14ed32dfb66c0b838024c25
JuanManceraL <167704417+JuanManceraL@users.noreply.github.com>
2024-11-10 09:22:08 -0600

    Correction Prints comments

--- Syntax.py
+++ Syntax.py
@@ -10,15 +10,15 @@ symbol_table = {}
 def p_program(p):
     """statements   : statements statement
                     | statement"""
-    print(f"P -> S")
-    print(p[1])
+    print(f"P <- S")
+    #print(p[1])
 
 def p_statement(p):
     """statement    : declaration
                     | assignment
                     | prt"""
-    print(f"S -> S")
-    p[0] = p[1]
+    print(f"S <- S")
+    #p[0] = p[1]
 
 def p_declaration(p):
     """declaration  : TYPE IDENTIFIER SEMIC"""
@@ -28,11 +28,11 @@ def p_declaration(p):
     else:
         symbol_table[var_name] = None
         print(f"Declaración: {var_name}")
-    print(f"D -> {p[1]}{p[2]}{p[3]}")
+    print(f"D(S) <- {p[1]} {p[2]}{p[3]}")
 
 def p_assignment(p):
     """assignment   : IDENTIFIER EQUALS expression SEMIC"""
-    print(f"A -> {p[1]}{p[2]}{p[3]}{p[4]}")
+    print(f"A(S) <- {p[1]}{p[2]}{p[3]}{p[4]}")
     var_name = p[1]
     if var_name not in symbol_table:
         print(f"Error: La variable '{var_name}' no está declarada.")
@@ -42,36 +42,35 @@ def p_assignment(p):
 
 def p_print(p):
     """prt  : PRINT LPAREN expression RPAREN SEMIC"""
-    print("Imprimiendo")
+    print(f"P(S) <- Imprimiendo... Printf({p[3]})")
     p[0] = p[1]
 
 #Syntax for summ
 def p_expression_plus(p):
     """expression   : expression PLUS term"""
-    #p[0] = p[1] + p[3]
-    print("Si se pudo burro")
-    #print(f"{p[1]}{p[2]}{p[3]}{p[4]}")
+    p[0] = p[1] + p[3]
+    print(f"E <- {p[1]}{p[2]}{p[3]}")
 
 def p_term_times(p):
     """term     : term TIMES factor"""
     p[0] = p[1] * p[3]
-    print(f"{p[1]} * {p[3]}")
+    print(f"T <- {p[1]} * {p[3]}")
 
 
 def p_factor_num(p):
     """factor   : NUMBER"""
     p[0] = p[1]
-    print(f"F -> N {p[1]}")
+    print(f"F <- N {p[1]}")
 
 def p_expression_term(p):
     """expression   : term""" #expression
-    #p[0] = p[1]
-    print(f"E -> T {p[1]}")
+    p[0] = p[1]
+    print(f"E <- T {p[1]}")
 
 def p_term_factor(p):
     "term : factor"
     p[0] = p[1]
-    print(f"T -> F {p[1]}")
+    print(f"T <- F {p[1]}")
 
 #def p_expression_term(p):
 #    """expression : term"""
--- ejemplo.c
+++ ejemplo.c
@@ -1,4 +1,4 @@
 int a;
 int b;
 a = 3 + 3;
-printf(5);
+printf(5+3*45*2);
