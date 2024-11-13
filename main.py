fba1a44500db3e567130689de8e0ba859583bfe8
JuanManceraL <167704417+JuanManceraL@users.noreply.github.com>
2024-11-13 06:44:54 -0600

    Upd Dir
    Update access to archives

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from pathlib import Path
 from Syntax import parser
+
 def leer_archivo(ruta_archivo):
     with open(ruta_archivo, 'r') as archivo:
         return archivo.read()
 
-ruta_archivo = 'C:/Users/juanm/Documents/Escuela/7mo semestre/Compiladores/Syntax_Semantica_Analyzer_Team_5/Syntax-Semantic-Analyzer-Team-05-/ejemplo.c'
-codigo = leer_archivo(ruta_archivo)
+script_dir = Path(__file__).parent
+
+input_file_name = "ejemplo.c"
+input_file_dir = script_dir / input_file_name
+
+#ruta_archivo = 'C:/Users/juanm/Documents/Escuela/7mo semestre/Compiladores/Syntax_Semantica_Analyzer_Team_5/Syntax-Semantic-Analyzer-Team-05-/ejemplo.c'
+codigo = leer_archivo(input_file_dir)
 
 # Ejecutar el analizador
 parser.parse(codigo)
